19b68506ce4c97f9b4f7a4f8f23bef25d341a46c
angelmanriqueg9-png <angelmanriqueg9@gmail.com>
2026-05-06 18:56:23 +0200

    pantallas

---
+++ proyecto 3eva/Assets/scripst/botones_1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class botones_1 : MonoBehaviour
+{
+    public void LoadScene(int sceneToLoad)
+    {
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Has cerrado el juego");
+        Application.Quit();
+    }
+}
